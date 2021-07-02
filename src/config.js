449f20c3811e6d6993e15f5c4599dff75d1632f4
Devesh Rawat <devsrawt@gmail.com>
2021-07-02 20:29:11 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const configs = {
-    RPC_URI: "https://rpc-mumbai.maticvigil.com/v1/97cc18f76e6f0c10540f666abe40b631d61d8335",
+    RPC_URI: "https://rpc-mumbai.maticvigil.com/v1/97cc18f76e6f0c10540f666abe40b631d61d83388",
     CHAIN_ID: 80001,
     TIME_INTERVEL: 30,
 };
